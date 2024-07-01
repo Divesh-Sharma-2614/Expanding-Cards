20ec7f0d55624f1bb9f20339e74c84ba0a4677de
divesh-sharma-2614 <diveshsharma2614@gmail.com>
2024-07-01 07:42:15 +0530

    All set

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll(".panel");
+
+// #forEach() is a higher order array method , these method takes function as argument
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
